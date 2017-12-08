6efc5b4eda4bfd73b458fda6bc8d9bb1cac2ee5e
Razzwan <razvanlomov@gmail.com>
2017-12-08 03:29:59 +0200

    fix eslint config

--- config/eslint.config.js
+++ config/eslint.config.js
@@ -5,7 +5,8 @@ module.exports = {
 
   extends: [
     "standard",
-    "standard-react"
+    "standard-react",
+    "prettier"
   ],
 
   plugins: ['import', 'flowtype', 'jsx-a11y', 'react', 'jest'],
@@ -30,23 +31,24 @@ module.exports = {
   },
 
   rules: {
-    "quotes": "off",
-    "semi": [2, "never"],
-    "comma-dangle": [2, "always-multiline"],
-    "space-infix-ops": 0,
-    "max-len": [2, 120, 2],
-    "react/jsx-no-bind": [1, {
-      "allowArrowFunctions": true
-    }],
-    "object-curly-spacing": "off",
-    "no-class-assign": "off",
-    "operator-linebreak": "off",
-    "no-octal-escape": "off",
-    "no-throw-literal": "off",
-    "camelcase": ["error", {"properties": "never"}],
-    "jest/no-disabled-tests": "warn",
-    "jest/no-focused-tests": "error",
-    "jest/no-identical-title": "error",
-    "jest/valid-expect": "error"
+    "space-before-function-paren": ["error", "never"],
+    // "quotes": "off",
+    // "semi": [2, "never"],
+    // "comma-dangle": [2, "always-multiline"],
+    // "space-infix-ops": 0,
+    // "max-len": [2, 120, 2],
+    // "react/jsx-no-bind": [1, {
+    //   "allowArrowFunctions": true
+    // }],
+    // "object-curly-spacing": "off",
+    // "no-class-assign": "off",
+    // "operator-linebreak": "off",
+    // "no-octal-escape": "off",
+    // "no-throw-literal": "off",
+    // "camelcase": ["error", {"properties": "never"}],
+    // "jest/no-disabled-tests": "warn",
+    // "jest/no-focused-tests": "error",
+    // "jest/no-identical-title": "error",
+    // "jest/valid-expect": "error"
   },
 }
